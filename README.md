8372473a793128d926edc20645e0d8bb2d39d9ee
Lawrenciumbc <lawrenciumbc@hotmail.com>
2020-04-14 00:46:31 +0100

    lots of fixes inc rt2msg addition and times commas

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # rds-command-gen
 
-Command generator for RDS schedules for RDS Pro based on Excel file input. Designed for use with RDS Pro GUI Command Line.
+Command generator for RDS schedules for the Audessence RDS Pro 2 encoder based on Excel file input. Designed for use with RDS Pro telnet command line and RDS PRO GUI (for RT2 Messages). This may work for other RDS encoders if they take similar commands. Please check your own device ofr compatibility.
 
 This is to help make keeping RDS schedules uptodate easier so somebody non-technical can do it. Someone who has access to the station's RDS encoder box can then paste the generated commands straight in and send to the box reducing technical overhead.
+
+This script currently doesn't auto send the commands via telnet. This is something that will be added with a config setting to toggle on and off. As the script was intended to keep the decoupling between the xlsx file and the actual changing of data on the encoder, this was not initially included - mainly to mitigate formatting errors and force a 'second set of eyes' on the commands before sending them.
 
 ## Install instructions
 
@@ -16,8 +18,30 @@ This is to help make keeping RDS schedules uptodate easier so somebody non-techn
 - cd into the directory of the repo
 - run 'node index.js'
 
+Also, don't use "bzTaEq" as a string within the show name on the xlsx. This string combination is used to encode commas between times to protect them temporarily. Unlikely but just a heads up!
+
+## Important Notes
+
+### Long Show Names
+
+The shown name max length is normally 64 characters BUT the command max length is 35 characters. The workaround (according to the encoder manual) is to add these longer titles as 'radiotext2 messages'. Therefore, this script generates the command to trigger an rt2msg corresponding to the show schedule entry number. There is then a separate txt file generated in the output folder (rt2Messages.txt) that lists the messages you need to manually add and under which message number to do so.
+
+This is a manual step because although a command for this can be generated, the gain would only be a few characters as the max command length is still 35. Doing it manually here allows you to make full use of the 64 character limit if required.
+
+If the show name is longer than 64 characters, it gets truncated to 61 characters and "..." gets added.
+
+### Custom PTY Codes
+
+Use the PTYN_config.json file to add custom PTY codes if required.
+
+This follows the format of:
+[{PTYN: {"32": "myCode", "33": "anotherCode"}}]
+The numbers start at 32 as upto 31 is taken by RDS standard PTY codes.
+
 ## ToDo
 
 - Create an install bat file
 - Create a run bat file
 - Add stricter parsing and error handling on the field inputs
+- Add telnet client and option to auto send to the box
+- Add 'watch' on a folder (probably within the bat file) for fully automated workflow (i.e. user just drops xlsx into that folder). This could be setup as a Dropbox or GDrive folder for remote triggering.
---
+++ src/index.js
@@ -0,0 +1,154 @@
+var fs = require("fs");
+var ptynCodes = require("./PTYN_config.json");
+
+const readXlsxFile = require("read-excel-file/node");
+
+const schema = {
+  SHOW: {
+    prop: "show",
+    type: String,
+    required: true,
+  },
+  DAYS: {
+    prop: "days",
+    type: String,
+    required: true,
+  },
+  TIMES: {
+    prop: "times",
+    type: String,
+    required: true,
+  },
+  GENRE: {
+    prop: "genre",
+    type: Number,
+  },
+};
+
+async function genCommands(inFile, msgFile) {
+  console.log("Generated Commands");
+  // need to return a promise here!
+  try {
+    const gennedCommands = await readXlsxFile(inFile, { schema }).then(
+      ({ rows, errors }) => {
+        // `rows` is an array of rows
+        // each row being an array of cells.
+
+        // `errors` have shape `{ row, column, error, value }`.
+        errors.length === 0;
+
+        let validRowCount = 0;
+        const commandsList = rows.map((r) => {
+          // r is the row
+
+          if (r && r.show && validRowCount <= 48) {
+            let showName = r.show;
+            const times = r.times;
+            const days = r.days;
+            const genre = r.genre;
+
+            let showCode = "RT1";
+
+            if (genre > 31) {
+              showCode = "PTYN";
+              showName = () => {
+                const parsedCodes = JSON.parse(ptynCodes);
+                return parsedCodes.PTYN.genre;
+              };
+            }
+
+            // the command max length is 35 chars. About 9 to 10 are taken up already.
+            // therefore if over 25 then write it to a message instead and recall the message.
+            // like RT1 text, messages can be 64 chars but command still has to be under 35
+            // so its better to use the output rt2messages.txt file to manually add these as messages.
+            if (showName.length > 25) {
+              let originalShowName = showName;
+
+              if (originalShowName.length > 64) {
+                originalShowName = originalShowName.substring(0, 61) + "...";
+              }
+
+              showName = `RT2MSG=${validRowCount + 1}`;
+
+              // add to message file
+              fs.appendFileSync(
+                msgFile,
+                `Message: ${validRowCount + 1}, ${originalShowName}\n`,
+                function (err) {
+                  if (err) throw err;
+                  console.log("Message required note Saved!");
+                }
+              );
+            }
+
+            // encode times commas so they don't get stripped out later. "bzTaEq" unlikely to be used in a showname
+            const encodedTimes = times.replace(/,/g, "bzTaEq");
+
+            const commandEntry =
+              `*S${validRowCount + 1}C=${showCode}=${showName}\n` +
+              `*S${validRowCount + 1}P=${genre}\n` +
+              `*S${validRowCount + 1}T=${encodedTimes}\n` +
+              `*S${validRowCount + 1}D=${days}\n`;
+
+            validRowCount++;
+
+            return commandEntry;
+          }
+          return;
+        }); // end map
+
+        return commandsList;
+      }
+    ); // end readfile
+
+    return gennedCommands;
+  } catch (e) {
+    console.log("error occured in gencommands function");
+    console.log(e);
+  }
+}
+
+function end(createdCommands, outFile) {
+  try {
+    const sendCommand = "*SEN=1\n*SEN\n";
+
+    const closeCommands = createdCommands.concat(sendCommand);
+
+    const stringCommands = closeCommands.toString();
+
+    // strip out commas
+    const stringCommandsClean = stringCommands.replace(/,/g, "");
+
+    //decode times commas encoding for final output
+    const stringFixTimesClean = stringCommandsClean.replace(/bzTaEq/g, ",");
+
+    fs.writeFile(outFile, stringFixTimesClean, function (err) {
+      if (err) throw err;
+      console.log("Commands Saved!");
+    });
+  } catch (e) {
+    console.log("error occured in end function");
+    console.log(e);
+  }
+}
+
+async function init() {
+  // import xlsx and setup output files
+  const scheduleIn = "../input/rds_template.xlsx";
+  const scheduleOut = "../output/rds_schedule.txt";
+  const msgFile = "../output/rt2Messages.txt";
+
+  // clean up prep
+  if (fs.existsSync(scheduleOut)) {
+    fs.unlinkSync(scheduleOut);
+  }
+  if (fs.existsSync(msgFile)) {
+    fs.unlinkSync(msgFile);
+  }
+
+  const commands = await genCommands(scheduleIn, msgFile);
+
+  end(commands, scheduleOut);
+}
+
+init();
