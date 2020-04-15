e682a272db5072eaa9d0207b6425c6ae806fc68d
Lawrenciumbc <lawrenciumbc@hotmail.com>
2020-04-15 01:54:00 +0100

    connection for each line

--- src/helpers/telnetSender.js
+++ src/helpers/telnetSender.js
@@ -7,35 +7,30 @@ const readline = require("readline");
 const sendViaTelnet = async (configs, commandsFile, responseFile) => {
   let connection = new Telnet();
 
+  const telnetConfigs = configs[0];
+
   const readInterface = readline.createInterface({
     input: fs.createReadStream(commandsFile),
     output: process.stdout,
     console: false,
   });
 
-  const telnetConfigs = configs[0];
-
-  // setup telnet connection
-  try {
-    const connectRes = await connection.connect(telnetConfigs);
-    console.log(`telnet connection successful, ${connectRes}`);
-  } catch (error) {
-    // handle the throw (timeout)
-    console.log(new Error(`error connecting to device via Telnet ${error}`));
-  }
-
   // send command, write response and command to file, delay of 250ms
   // delay is to give the encoder chance to acknowledge the command
   readInterface.on("line", async function (line) {
     // setTimeout(async function () {
-    console.log(line);
+
+    // setup telnet connection
+    try {
+      const connectRes = await connection.connect(telnetConfigs);
+      console.log(`telnet connection successful, ${connectRes}`);
+    } catch (error) {
+      // handle the throw (timeout)
+      console.log(new Error(`error connecting to device via Telnet ${error}`));
+    }
 
     const trimmedLine = line.trim();
-    console.log("trimmed line =", trimmedLine);
     const res = await connection.exec(trimmedLine);
-
-    // let res = await connection.exec("uptime");
-    // console.log("async result:", res);
 
     fs.appendFileSync(
       responseFile,
